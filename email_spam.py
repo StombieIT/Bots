a4d33a63c310e6c7709e5510d74d809542f3884e
StombieIT <65386839+StombieIT@users.noreply.github.com>
2020-05-25 11:05:34 +0300

    Add exceptions of many errors
    Besides, add new smtp protocols.

--- email_spam.py
+++ email_spam.py
@@ -1,35 +1,46 @@
-import smtplib as root
+import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from time import sleep
 def send_mail():
 	login = input('Type your mail: ')
-	"""
-	if login.endswith('@yandex.ru') == True:
-		url = 'smtp.yandex.ru'
-	elif login.endswith('@mail.ru') == True:
+	if login.endswith('@mail.ru') == True:
 		url = 'smtp.mail.ru'
+	elif login.endswith('@yandex.ru') == True:
+		url = 'smtp.yandex.ru'
 	elif login.endswith('@inbox.ru') == True:
 		url = 'smtp.inbox.ru'
-	elif login.endswith('@gmail.com') == True:
-		url = 'smtp.gmail.com'
-	"""
-	url = input('Type URL: ')
+	elif login.endswith('@list.ru') == True:
+		url = 'smtp.list.ru'
+	elif login.endswith('@bk.ru') == True:
+		url = 'smtp.bk.ru'
+	else:
+		url = input('Type URL: ')
+	server = smtplib.SMTP_SSL(url, 465)
 	password = input('Type password of your mail: ')
-	toaddr = input('Type mail of victime: ')
-	topic = input('Type topic: ')
-	body = input('Type message: ')
-	msg = MIMEMultipart()
-	msg['Subject'] = topic
-	msg['From'] = login
-	msg.attach(MIMEText(body, 'plain'))
-	server = root.SMTP_SSL(url, 465)
-	server.login(login, password)
-	for i in range(int(input('How many times: '))):
-		server.sendmail(login, toaddr, msg.as_string())
-		print('Sending ' + str(i + 1) + ' message to '  + toaddr)
-		sleep(10)
-	server.quit()
-	print('Done')
+	try:
+		server.login(login, password)
+	except smtplib.SMTPAuthenticationError:
+			print('Failed to log in')
+	else:
+		toaddr = input('Type mail of victime: ')
+		topic = input('Type topic: ')
+		body = input('Type message: ')
+		msg = MIMEMultipart()
+		msg['Subject'] = topic
+		msg['From'] = login
+		msg.attach(MIMEText(body, 'plain'))
+		try:
+			for i in range(int(input('How many times: '))):
+				server.sendmail(login, toaddr, msg.as_string())
+				print('Sent ' + str(i + 1) + ' message to ['  + toaddr + ']')
+		except smtplib.SMTPRecipientsRefused:
+			print('Failed to find [' + toaddr + ']')
+		except KeyboardInterrupt:
+			print('Aborted')
+		except smtplib.SMTPDataError:
+			print('Failed to send message to [' + toaddr + ']')
+		else:
+			print('Done')
+		server.quit()
 if __name__ == '__main__':
 	send_mail()
